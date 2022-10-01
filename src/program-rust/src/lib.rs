ee57c783ec82d6b64cafc37a91f2733e54c23dec
SuluKarpuz <dilmacefe@gmail.com>
2022-10-01 14:17:21 +0300

    patika task done

--- src/program-rust/src/lib.rs
+++ src/program-rust/src/lib.rs
@@ -27,15 +27,23 @@ pub fn process_instruction(
     msg!("Hello World Rust program entrypoint");
 
     // Iterating accounts is safer than indexing
-  
+    let accounts_iter = &mut accounts.iter();
 
     // Get the account to say hello to
+    let account = next_account_info(accounts_iter)?;
    
 
     // The account must be owned by the program in order to modify its data
-   
+    if account.owner != program_id {
+        msg!("Greeted account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
+    }
 
     // Increment and store the number of times the account has been greeted
+    let mut greeting_account = GreetingAccount::try_from_slice(&account.data.borrow())?;
+    //changed the counter to 10
+    greeting_account.counter += 10;
+    greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
 
     msg!("Greeted {} time(s)!", greeting_account.counter);
 
